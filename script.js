b72feca75719c52fae3f86d626bd11e95921d77a
Owiec311 <106275317+Owiec311@users.noreply.github.com>
2026-05-09 23:15:56 +0200

    Refactor smileEffect function for clarity

--- script.js
+++ script.js
@@ -21,10 +21,7 @@ function smileEffect() {
     // ==========================================
     // ZMIEŃ 192.168.1.50 na adres IP komputera
     // lub Raspberry Pi, na którym działa app.py
-    fetch("https://concord-net-realize-occurs.trycloudflare.com/smile", {
-    mode: "cors",
-    cache: "no-store"
-    })
+    fetch("https://concord-net-realize-occurs.trycloudflare.com/smile")
         .catch(error => console.log("Błąd połączenia z serwerem:", error));
     // Znajdź przycisk i zablokuj go
     const button = document.querySelector(".secondary");
