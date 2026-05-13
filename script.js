b23180ddc5b32bcf63a3168a3d363b877380e777
Owiec311 <106275317+Owiec311@users.noreply.github.com>
2026-05-13 20:44:24 +0200

    Update server URL in smileEffect function

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function smileEffect() {
     // WYWOŁANIE SERWERA FLASK W TLE
     // ==========================================
     // Nie czekamy na odpowiedź, więc animacja uruchamia się od razu.
-    fetch("https://according-franchise-quarters-bond.trycloudflare.com/smile")
+    fetch("https://student-intention-tonight-actual.trycloudflare.com/smile")
         .catch(error => {
             console.log("Błąd połączenia z serwerem:", error);
         });
